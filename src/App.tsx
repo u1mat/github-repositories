e797188f91c7d53ca26290a8f483dd99d99e2fba
Urmat Kokonbaev <urmat.kokonbaev@pearson.com>
2025-10-14 23:43:31 +0600

    added api base, and fetch adapter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
+import { fetchApi } from './libs/api';
+
 function App() {
-  const testVariable = 'Hello, World';
-  return <>{testVariable}</>;
+  const currentProviderApi = fetchApi;
+  useEffect(() => {
+    void (async () => {
+      try {
+        const data = await currentProviderApi.get('/posts');
+        console.log(data);
+      } catch (error) {
+        console.error('Failed to fetch posts:', error);
+      }
+    })();
+  }, [currentProviderApi]);
+  return <></>;
 }
 
 export default App;
